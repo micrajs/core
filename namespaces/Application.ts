6dc6939e71cf9e722f7901b7c2a33f8928e76e96
Olavo Amorim Santos <olavo.a.santos@gmail.com>
2022-03-14 11:30:42 -0500

    docs: improved Application namespace docblock

--- namespaces/Application.ts
+++ namespaces/Application.ts
@@ -1,7 +1,7 @@
 declare global {
   /**
    * The Application is a configurable and extensible namespace that can be used to define application-wide interfaces and functions.
-   * This allows modules to be written in a modular fashion and to be easily extensible.
+   * This allows modules to be written in a modular fashion, to be easily extensible and maintain type safety.
    */
   namespace Application {}
 }
