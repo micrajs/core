ea191633d0c91fd7d8c23e4c8034fe4a04366890
Olavo Amorim Santos <olavo.a.santos@gmail.com>
2022-03-14 19:20:36 -0500

    chore: updated developer tools to use multiple packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,10 +1,3 @@
-// eslint-disable-next-line @typescript-eslint/no-var-requires
-const baseConfig = require('@micra/developer-tools/eslint/library');
-
 module.exports = {
-  ...baseConfig,
-  rules: {
-    ...baseConfig.rules,
-    '@typescript-eslint/no-empty-interface': 'off',
-  },
+  extends: ['@micra/eslint-config/typescript'],
 };
