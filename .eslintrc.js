22ab5293259ca7bf582bf91407229e55c913bdfd
Olavo Amorim Santos <olavo.a.santos@gmail.com>
2022-03-18 17:40:51 -0500

    chore: disabled the @typescript-eslint/no-empty-interface lint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,7 @@
 module.exports = {
   extends: ['@micra/eslint-config/typescript'],
+  rules: {
+    // Sometimes it is necessary to use empty interfaces to define extensible types.
+    '@typescript-eslint/no-empty-interface': 'off',
+  },
 };
