c63c152f0e328cd66751ec71afce8efd044e9644
Olavo Amorim Santos <olavo.a.santos@gmail.com>
2022-03-18 17:39:37 -0500

    feat: introduced Micra.ValidationError interface

--- error/index.ts
+++ error/index.ts
@@ -55,6 +55,76 @@ declare global {
        */
       serialize(): ErrorMessage[];
     }
+
+    /**
+     * This interface describes the standard validation error class that can be thrown by the framework.
+     */
+    interface ValidationError<Fields extends string> extends Error {
+      /**
+       * The HyperText Transfer Protocol (HTTP) 422 Unprocessable Entity response status code indicates that the server understands the content type of the request entity, and the syntax of the request entity is correct, but it was unable to process the contained instructions.
+       *
+       * @link https://developer.mozilla.org/en-US/docs/Web/HTTP/Status/422
+       */
+      statusCode: 422;
+
+      /**
+       * Checks if there are any validation errors.
+       *
+       * @returns `true` if there are validation errors, otherwise `false`.
+       */
+      hasAny(): boolean;
+
+      /**
+       * Checks if there are any validation errors for a given field.
+       *
+       * @param field - The field to check.
+       * @returns `true` if there are validation errors for the given field, otherwise `false`.
+       */
+      has(field: Fields): boolean;
+
+      /**
+       * Returns the validation errors for a given field.
+       *
+       * @param field - The field to check.
+       * @returns The validation errors for the given field or `undefined` if there are no validation errors or the field doesn't exist.
+       */
+      get(field: Fields): ValidationErrorExtras<Fields> | undefined;
+
+      /**
+       * Defines the validation errors for a given field.
+       *
+       * @param field - The field to check.
+       * @param extras - The extras to be added to the validation error.
+       *
+       * @returns The ValidationError instance.
+       */
+      set<Field extends Fields>(
+        field: Field,
+        extras: ValidationErrorExtras<Field>,
+      ): this;
+    }
+
+    /**
+     * It describes the extra information that can be attached to a validation error message.
+     * This is used to provide additional information to the user and is serialized
+     * as the `extra` property of the error message.
+     */
+    interface ValidationErrorExtras<Fields extends string> {
+      /**
+       * The field that caused the error.
+       */
+      field: Fields;
+
+      /**
+       * The error message.
+       */
+      message: string;
+
+      /**
+       * Optional extra information.
+       */
+      variables?: Record<string, unknown>;
+    }
   }
 }
 
