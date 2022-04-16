9ea164df7ccff04d2ffeb870a361a98b5703232a
Olavo Amorim Santos <olavo.a.santos@gmail.com>
2022-04-16 17:32:03 -0500

    feat: improved types for service-container and event-emitter

--- service-container/index.ts
+++ service-container/index.ts
@@ -43,6 +43,33 @@ declare global {
     }
 
     /**
+     * It defines a given service.
+     */
+    interface ServiceDefinition<Namespace extends keyof Application.Services> {
+      dependencies: Exclude<keyof Application.Services, Namespace>[];
+      namespace: Namespace;
+      value: Static<Application.Services[Namespace], any[], any>;
+    }
+
+    /**
+     * It defines a given factory that creates a service.
+     */
+    interface FactoryServiceDefinition<
+      Namespace extends keyof Application.Services,
+    > {
+      factory: ServiceFactory<Namespace>;
+      isSingleton: boolean;
+      namespace: Namespace;
+    }
+
+    /**
+     * It creates a given service.
+     */
+    type ServiceFactory<Namespace extends keyof Application.Services> = (
+      container: ServiceContainer,
+    ) => Application.Services[Namespace];
+
+    /**
      * The ServiceContainer entity is responsible for storing, instantiating and retrieving service instances for the application. It extends the EventEmitter class and provides a set of methods to interact with the services.
      */
     interface ServiceContainer
@@ -62,6 +89,23 @@ declare global {
         namespace: Namespace,
         service: Static<Application.Services[Namespace]>,
       ): this;
+      /**
+       * It registers a service with the container as a non-shared instance. Such service will be instantiated every time it is requested. The service definition can be used to define the dependencies of the service, which will be resolved when the service is requested.
+       *
+       * @param serviceDefinition - Object that defines the service.
+       * @returns The container instance.
+       *
+       * @emits ServiceContainerEvents.set
+       *
+       * @typeParam `Namespace` - The namespace of the service defined in the Application.Services interface. Inferred from the `namespace` parameter.
+       */
+      register<Namespace extends keyof Application.Services>(
+        serviceDefinition: ServiceDefinition<Namespace>,
+      ): this;
+      register<Namespace extends keyof Application.Services>(
+        namespaceOrServiceDefinition: Namespace | ServiceDefinition<Namespace>,
+        service?: Static<Application.Services[Namespace]>,
+      ): this;
 
       /**
        * It registers a service with the container as a shared instance. Such service will be instantiated only once and then shared among all the requests.
@@ -78,21 +122,22 @@ declare global {
         namespace: Namespace,
         service: Static<Application.Services[Namespace]>,
       ): this;
-
       /**
-       * It registers a service with the container as a shared instance. Such service will not be instantiated and will be returned as defined.
+       * It registers a service with the container as a shared instance. Such service will be instantiated only once and then shared among all the requests. The service definition can be used to define the dependencies of the service, which will be resolved when the service is requested.
        *
-       * @param namespace - The namespace of the service.
-       * @param value - The service to be registered.
+       * @param serviceDefinition - Object that defines the service.
        * @returns The container instance.
        *
        * @emits ServiceContainerEvents.set
        *
        * @typeParam `Namespace` - The namespace of the service defined in the Application.Services interface. Inferred from the `namespace` parameter.
        */
-      value<Namespace extends keyof Application.Services>(
-        namespace: Namespace,
-        value: Application.Services[Namespace],
+      singleton<Namespace extends keyof Application.Services>(
+        serviceDefinition: ServiceDefinition<Namespace>,
+      ): this;
+      singleton<Namespace extends keyof Application.Services>(
+        namespaceOrServiceDefinition: Namespace | ServiceDefinition<Namespace>,
+        service?: Static<Application.Services[Namespace]>,
       ): this;
 
       /**
@@ -108,9 +153,40 @@ declare global {
        */
       factory<Namespace extends keyof Application.Services>(
         namespace: Namespace,
-        factory: (
-          dependencyContainer: ServiceContainer,
-        ) => Application.Services[Namespace],
+        factory: ServiceFactory<Namespace>,
+      ): this;
+      /**
+       * It registers a service factory with the container that returns the instance of a service. The service definition can be used to define is this service should be resolved as a singleton or not.
+       *
+       * @param serviceDefinition - Object that defines the service.
+       * @returns The container instance.
+       *
+       * @emits ServiceContainerEvents.set
+       *
+       * @typeParam `Namespace` - The namespace of the service defined in the Application.Services interface. Inferred from the `namespace` parameter.
+       */
+      factory<Namespace extends keyof Application.Services>(
+        serviceDefinition: FactoryServiceDefinition<Namespace>,
+      ): this;
+      factory<Namespace extends keyof Application.Services>(
+        namespaceOrServiceDefinition: Namespace | ServiceDefinition<Namespace>,
+        factory?: ServiceFactory<Namespace>,
+      ): this;
+
+      /**
+       * It registers a service with the container as a shared instance. Such service will not be instantiated and will be returned as defined.
+       *
+       * @param namespace - The namespace of the service.
+       * @param value - The service to be registered.
+       * @returns The container instance.
+       *
+       * @emits ServiceContainerEvents.set
+       *
+       * @typeParam `Namespace` - The namespace of the service defined in the Application.Services interface. Inferred from the `namespace` parameter.
+       */
+      value<Namespace extends keyof Application.Services>(
+        namespace: Namespace,
+        value: Application.Services[Namespace],
       ): this;
 
       /**
