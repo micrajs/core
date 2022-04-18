50dcf5b83ac2fb1c592eef2d6d45998bbf2220da
Olavo Amorim Santos <olavo.a.santos@gmail.com>
2022-04-18 16:48:52 -0500

    feat: created the Service Provider contract

---
+++ service-provider/index.ts
@@ -0,0 +1,24 @@
+declare global {
+  namespace Micra {
+    /**
+     * It registers and initializes a service on the service container.
+     */
+    interface ServiceProvider {
+      /**
+       * It registers a service on the service container. This method should not be used to retrieve a service from the container as it is not guaranteed that the service is already registered.
+       *
+       * @param container - The service container.
+       */
+      register?(container: ServiceContainer): void | Promise<void>;
+
+      /**
+       * It initializes a service on the service container. This method is called once every service providers' register method have been called.
+       *
+       * @param container - The service container.
+       */
+      boot?(container: ServiceContainer): void | Promise<void>;
+    }
+  }
+}
+
+export {};
