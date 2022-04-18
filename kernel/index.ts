5de34dae394e80093ce2f01014bf3fb7dbd6fde6
Olavo Amorim Santos <olavo.a.santos@gmail.com>
2022-04-18 17:11:49 -0500

    feat: created the Kernel contract

---
+++ kernel/index.ts
@@ -0,0 +1,22 @@
+declare global {
+  namespace Micra {
+    /**
+     * Kernels are the core of a Micra application. These classes are responsible for determining how a given application should be executed based on all the services available.
+     *
+     * @typeParam Return - The return type of the kernel's run method.
+     */
+    interface Kernel<Return = void> {
+      /**
+       * It initializes the kernel. This method is called after all of the application's services have been registered and booted. With this, the kernel can perform any necessary initialization based on all the services available.
+       */
+      boot?(container: ServiceContainer): void | Promise<void>;
+
+      /**
+       * It runs the kernel. This method is responsible for defining how the application is executed.
+       */
+      run?(container: ServiceContainer): Return | Promise<Return>;
+    }
+  }
+}
+
+export {};
