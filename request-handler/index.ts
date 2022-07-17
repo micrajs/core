a29e125519997f568806dedc1cc2f97a7293e1ef
Olavo Amorim Santos <olavo.a.santos@gmail.com>
2022-07-17 09:17:26 -0500

    feat: added request-handler and ui-engine definitions

---
+++ request-handler/index.ts
@@ -0,0 +1,51 @@
+import '@/namespaces';
+
+declare global {
+  namespace Micra {
+    /**
+     * It defines the engine responsible for rendering the application's UI.
+     */
+    interface UIEngine<ComponentType> {
+      /**
+       * It registers a component with the engine. This component will be used
+       * to mount or render the application's UI to string.
+       *
+       * @param root The root component of the application.
+       */
+      setRoot(root: ComponentType): this;
+
+      /**
+       * It mounts the application's UI to the given element. If given a component,
+       * it will mount the component to the given element. If no component is given
+       * it will mount the root component.
+       *
+       * @param element The element to mount the application's UI to.
+       * @param component An optional component to render. If not provided, the engine will render the root component.
+       * @param props An optional props object to pass to the component.
+       */
+      mount<
+        Element extends globalThis.Element | DocumentFragment,
+        Props extends Record<string, any>,
+      >(
+        element: Element,
+        component?: ComponentType,
+        props?: Props,
+      ): void;
+
+      /**
+       * It renders the application's UI to string. If given a component,
+       * it will render the component to string. If no component is given
+       * it will render the root component.
+       *
+       * @param component An optional component to render. If not provided, the engine will render the root component.
+       * @param props An optional props object to pass to the component.
+       */
+      toString<Props extends Record<string, any>>(
+        component?: ComponentType,
+        props?: Props,
+      ): string;
+    }
+  }
+}
+
+export {};
---
+++ ui-engine/index.ts
@@ -0,0 +1,37 @@
+import '@/namespaces';
+import '@/service-container';
+import '@/application/globals/helpers';
+
+declare global {
+  namespace Micra {
+    /**
+     * It defines the context of a given request handler.
+     */
+    interface RequestHandlerContext {
+      request: Request;
+      config: Config;
+      env: Env;
+      use: Use;
+    }
+
+    /**
+     * Request handlers are functions that are responsible for handling a request.
+     * They are registered in the RequestHandlerManager and are executed in the
+     * order they are registered.
+     */
+    type RequestHandler = (
+      context: RequestHandlerContext,
+    ) => Promise<Response | void>;
+
+    /**
+     * It manages a list of request handlers which can be extended by custom router extensions.
+     * This allows extensions to define a particular way a given request type is handled.
+     */
+    interface RequestHandlerManager {
+      use(...handlers: RequestHandler[]): this;
+      handle(context: RequestHandlerContext): Promise<Response>;
+    }
+  }
+}
+
+export {};
